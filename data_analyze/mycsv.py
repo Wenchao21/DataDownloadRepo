86c0214902c2ed4fab3ce654aca68816da1febd3
Wenchao He <hewe@ub160403dktp64.amd.com>
2017-10-24 17:39:42 +0800

    update the readme

--- data_analyze/mycsv.py
+++ data_analyze/mycsv.py
@@ -2,19 +2,42 @@ import csv
 import json
 import jieba
 import jieba.analyse
+
+decade = []
+title = []
+author_name = []
 with open('result.csv') as f:
     reader = csv.reader(f)
-    string1 = ""
     for wordsss in reader:
-        seg_list = jieba.cut(str(wordsss[1]), cut_all=False)
-        print("/".join(seg_list))
-        string1 = string1 + str("/".join(seg_list))
+#        print(wordsss[0])
+        decade.append(wordsss[0])
+        title.append(wordsss[2])
+        author_name.append(wordsss[3])
+
+
+New_title = set(decade)
+New_title.remove('author_decade')
+
+list22 = list(New_title)
+num = len(New_title)
+print("All decae: %s, total: %s"%(list22,num))
+
+countsong = 0
+
+for i in range(0, len(New_title)-1):
+    for j in range(0, len(decade)-1):
+        if str(decade[j]) == str(list22[i]):
+            countsong = countsong + 1
+print(countsong)
+#        seg_list = jieba.cut(str(wordsss[0]), cut_all=False)
+#        print("/".join(seg_list))
+#        string1 = string1 + str("/".join(seg_list))
 
 #cipin fenxi
 
-tags = jieba.analyse.extract_tags(string1, topK=100)
-print(tags)
-print(','.join(seg_list))
+#tags = jieba.analyse.extract_tags(string1, topK=100)
+#print(tags)
+#print(','.join(seg_list))
 
 #import jieba.posseg
 #words = jieba.posseg.cut(string1)
